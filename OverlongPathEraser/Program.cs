2388ca7f60b88b13eb8fc1757e06b9ea95b09678
BlackMesa <admin@blackmesa.eu>
2017-03-08 17:11:56 +0100

    refactoring and cleanup

--- OverlongPathEraser/Program.cs
+++ OverlongPathEraser/Program.cs
@@ -13,47 +13,10 @@ namespace OverlongPathEraser
     {
         static void Main(string[] args)
         {
-            var proceed = true;
-
-            if (args == null || args.Length == 0)
+            if (ValidateArguments(args))
             {
-                Console.WriteLine("Missing argument: path to delete");
-                proceed = false;
-            }
-
-            Console.WriteLine("Passed arguments:");
-            for (int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine("\t" + args[i]);
-            }
-
-            if (args.Length > 1)
-            {
-                Console.WriteLine("Too many arguments: please pass only one path to delete");
-                proceed = false;
-            }
-
-            if (!IsValidPath(args[0]))
-            {
-                Console.WriteLine("Invalid argument: path to delete does not exist");
-                proceed = false;
-            }
-
-            if (!PathIsFolder(args[0]))
-            {
-                Console.WriteLine("Invalid argument: path needs to point to a folder");
-                proceed = false;
-            }
-
-            if (FolderIsRootFolder(args[0]))
-            {
-                Console.WriteLine("Invalid argument: path must not be a root folder / drive letter");
-                proceed = false;
-            }
-
-            if (proceed)
-            {
-                Console.Write("If you want to proceed type 'DELETE' without quotes and hit 'Enter':  ");
+                Console.WriteLine("Path to delete: " + args[0] + Environment.NewLine);
+                Console.WriteLine("If you want to proceed type 'DELETE' without quotes and hit 'Enter':  ");
                 var userResponse = Console.ReadLine();
 
                 if (userResponse == "DELETE")
@@ -72,7 +35,49 @@ namespace OverlongPathEraser
 
         }
 
-        
+        private static bool ValidateArguments(string[] args)
+        {
+            var argumentIsValid = true;
+
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Missing argument: path to delete");
+                argumentIsValid = false;
+            }
+            else
+            {
+                if (args.Length > 1)
+                {
+                    Console.WriteLine("Too many arguments: please pass only one path to delete");
+                    argumentIsValid = false;
+                }
+                else
+                {
+                    if (!IsValidPath(args[0]))
+                    {
+                        Console.WriteLine("Invalid argument: path to delete does not exist");
+                        argumentIsValid = false;
+                    }
+                    else
+                    {
+                        if (!PathIsFolder(args[0]))
+                        {
+                            Console.WriteLine("Invalid argument: path needs to point to a folder");
+                            argumentIsValid = false;
+                        }
+
+                        if (FolderIsRootFolder(args[0]))
+                        {
+                            Console.WriteLine("Invalid argument: path must not be a root folder / drive letter");
+                            argumentIsValid = false;
+                        }
+                    }
+                }
+            }
+
+            return argumentIsValid;
+        }
+
 
         public static bool IsValidPath(string path)
         {
@@ -107,15 +112,28 @@ namespace OverlongPathEraser
 
             var arguments = string.Format("\"{0}\" \"{1}\" /MIR", emptyTempDirectory.FullName, folderToDelete.FullName); // if /MIR is not working, try /purge ??
 
-            using (var process = Process.Start(new ProcessStartInfo("robocopy")
+            var processStartInfo = new ProcessStartInfo("Robocopy32")
             {
                 Arguments = arguments,
                 CreateNoWindow = true,
                 UseShellExecute = false,
-            }))
-            process.WaitForExit();
+                WorkingDirectory = Environment.CurrentDirectory
+            };
+
+            using (var process = Process.Start(processStartInfo))
+            {
+                var spinner = new Spinner();
+                spinner.Start();
+                process.WaitForExit();
+                spinner.Stop();
+            }
+
             folderToDelete.Delete();
             emptyTempDirectory.Delete();
         }
+
+
+
+
     }
 }
