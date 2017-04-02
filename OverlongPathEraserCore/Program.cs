03c392a949e70d763ff7b866e862d0b7f7ded9dd
Stephan Sum <sum@live.de>
2017-04-02 02:43:59 +0200

    continued refactoring; moved to dotnet core

---
+++ OverlongPathEraserCore/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OverlongPathEraser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (var path in args)
+            {
+                var overlongPath = new OverlongPath(path);
+
+                if (ConfirmDeletion(path))
+                {
+                    var erasureResult = overlongPath.Erase();
+
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine(erasureResult.Report());
+                    Console.Write(Environment.NewLine);
+                }
+            }
+
+            Console.WriteLine("Press 'Enter' to close...");
+            Console.ReadLine();
+
+        }
+
+
+        private static bool ConfirmDeletion(string path)
+        {
+            Console.WriteLine("Do you want to erase: " + path + "?" + Environment.NewLine);
+            Console.WriteLine("(Y)es or (N)o?");
+            var userResponse = Console.ReadLine();
+
+            if (userResponse.ToUpper() == "Y")
+                return true;
+
+            return false;
+        }
+
+
+
+    }
+}
