d2ff7623d1392ad6c121c5aa4209de34defe7660
Stephan Sum <sum@live.de>
2017-04-05 00:18:04 +0200

    removed abundant comments

--- OverlongPathEraser.Tests/OperationResultTests.cs
+++ OverlongPathEraser.Tests/OperationResultTests.cs
@@ -10,14 +10,11 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Report_GivenAListOfErrors_ShouldReturnAConcatedString()
         {
-            // Arrange
             var errors = new List<PathError>() { PathError.IsDriveLetter, PathError.IsNoFolder};
             var result = new OperationResult<PathError>(errors);
 
-            // Act
             var report = result.Report();
 
-            // Assert
             Assert.AreEqual(report, 
                 PathError.IsDriveLetter + Environment.NewLine + 
                 PathError.IsNoFolder + Environment.NewLine);
--- OverlongPathEraser.Tests/OverlongPathTests.cs
+++ OverlongPathEraser.Tests/OverlongPathTests.cs
@@ -10,14 +10,11 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Validate_GivenNonExistingDirectory_ReturnsWithError()
         {
-            // Arrange
             var path = @"X:\Just\A\Non\Existing\Directory";
             var overlongPath = new OverlongPath(path);
 
-            // Act
             var result = overlongPath.Validate();
 
-            // Assert
             Assert.IsTrue(result.Errors.Contains(PathError.NotExisting));
         }
 
@@ -25,14 +22,11 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Validate_GivenExistingHarddriveRoot_ReturnsWithError()
         {
-            // Arrange
             var path = @"C:\";
             var overlongPath = new OverlongPath(path);
 
-            // Act
             var result = overlongPath.Validate();
 
-            // Assert
             Assert.IsTrue(result.Errors.Contains(PathError.IsDriveLetter));
         }
 
@@ -40,14 +34,11 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Validate_GivenNoValidFolder_ReturnsWithError()
         {
-            // Arrange
             var path = Path.GetTempFileName(); // we are doing an integration test here instead of mocking the the folder validation
             var overlongPath = new OverlongPath(path);
 
-            // Act
             var result = overlongPath.Validate();
 
-            // Assert
             Assert.IsTrue(result.Errors.Contains(PathError.IsNoFolder));
 
             // Cleanup
@@ -58,16 +49,13 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Erase_GivenAValidedPath_ReturnsWithSuccess()
         {
-            // Arrange
             // we are doing an integration test here instead of mocking file system calls
             var path = Path.Combine(Path.GetTempPath(), "OverlongPathEraserTestFolder");
             Directory.CreateDirectory(path);
             var overlongPath = new OverlongPath(path);
 
-            // Act
             var result = overlongPath.Erase();
 
-            // Assert
             Assert.IsTrue(result.Success);
 
             // Cleanup (in case Erase() failed)
@@ -79,16 +67,13 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Erase_GivenAValidedPath_DeletesPath()
         {
-            // Arrange
             // we are doing an integration test here instead of mocking file system calls
             var path = Path.Combine(Path.GetTempPath(), "OverlongPathEraserTestFolder");
             Directory.CreateDirectory(path);
             var overlongPath = new OverlongPath(path);
 
-            // Act
             var result = overlongPath.Erase();
 
-            // Assert
             Assert.IsFalse(Directory.Exists(path));
 
             // Cleanup (in case Erase() failed)
@@ -100,14 +85,11 @@ namespace OverlongPathEraser.Tests
         [TestMethod]
         public void Erase_GivenAnInvalidPath_ReturnsWithoutSuccess()
         {
-            // Arrange
             var path = @"X:\Just\A\Non\Existing\Directory";
             var overlongPath = new OverlongPath(path);
 
-            // Act
             var result = overlongPath.Erase();
 
-            // Assert
             Assert.IsFalse(result.Success);
         }
 
